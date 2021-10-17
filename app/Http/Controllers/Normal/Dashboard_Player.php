ea268d456277ad45b586887e8d6b62dbfd098790
mccescario <201811926@fit.edu.ph>
2021-10-18 03:20:12 +0800

    Player Controller

---
+++ app/Http/Controllers/Normal/Dashboard_Player.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Normal;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Dashboard_Player extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Normal/Profile_management.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Normal;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Profile_management extends Controller
+{
+    //
+}
