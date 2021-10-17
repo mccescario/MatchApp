95dd9f9108703f2b9ea6678f92514bd65047f086
mccescario <201811926@fit.edu.ph>
2021-10-18 01:14:29 +0800

    laravel

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,10 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
-/*
-Route::group(['middleware' => 'auth'],function(){
 
-    Route::group(['middleware' => 'role:player', 'prefix'=> 'player', 'as' => 'player.'], function() {
+Route::group(['middleware' => 'auth'], function() {
+
+    Route::group(['middleware' => 'role:player', 'prefix' => 'player', 'as' => 'player.'], function() {
         Route::resource(name: 'dashboard_player', controller: \App\Http\Controllers\Normal\Dashboard_Player::class );
         Route::resource(name: 'profile', controller: \App\Http\Controllers\Normal\Profile_management::class );
         Route::resource(name: 'newsfeed', controller: \App\Http\Controllers\Normal\NewsfeedController::class );
@@ -33,14 +33,14 @@ Route::group(['middleware' => 'auth'],function(){
         Route::resource(name: 'schedule', controller: \App\Http\Controllers\Normal\ScheduleController::class );
         Route::resource(name: 'search', controller: \App\Http\Controllers\Normal\SearchController::class );
     });
-    Route::group(['middleware' => 'role:host', 'prefix'=> 'host', 'as' => 'host.'], function() {
+    Route::group(['middleware' => 'role:host', 'prefix' => 'host', 'as' => 'host.'], function() {
         Route::resource(name: 'dashboard_host', controller: \App\Http\Controllers\Host\Dashboard_Host::class );
         Route::resource(name: 'player-management', controller: \App\Http\Controllers\Host\Normal_management::class );
         Route::resource(name: 'schedule-management', controller: \App\Http\Controllers\Host\Schedule_management::class );
         Route::resource(name: 'stream-management', controller: \App\Http\Controllers\Host\Stream_management::class );
         Route::resource(name: 'tournament-management', controller: \App\Http\Controllers\Host\Tournament_management::class );
     });
-    Route::group(['middleware' => 'role:admin', 'prefix'=> 'admin', 'as' => 'admin.'], function() {
+    Route::group(['middleware' => 'role:admin', 'prefix' => 'admin', 'as' => 'admin.'], function() {
         Route::resource(name: 'dashboard_admin', controller: \App\Http\Controllers\Admin\Dashboard_Admin::class );
         Route::resource(name: 'user-management', controller: \App\Http\Controllers\Admin\User_management::class );
         Route::resource(name: 'role-management', controller: \App\Http\Controllers\Admin\Role_management::class );
@@ -49,4 +49,4 @@ Route::group(['middleware' => 'auth'],function(){
 
 
 });
-*/
+
