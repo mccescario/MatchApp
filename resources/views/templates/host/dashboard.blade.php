2c115de5a92af2792b4108a7335803804d7c0536
mccescario <201811926@fit.edu.ph>
2022-03-28 10:42:52 +0800

    Update dashboard.blade.php

--- resources/views/templates/host/dashboard.blade.php
+++ resources/views/templates/host/dashboard.blade.php
@@ -17,6 +17,8 @@
         </div>
     @endif
 
+
+
     <div class="row">
         <div class="col-lg-7 col-xl-8">
 
@@ -48,7 +50,7 @@
                                         <h1 class="text-center" style="font-family: Nunito, sans-serif;">{{ $news->title }}</h1>
                                         <p class="text-center">
                                             <span class="by" style="font-family: Nunito, sans-serif;">by</span>
-                                            <a href="#" style="font-family: Nunito, sans-serif;">{{ $news->user->name }}</a>
+                                            <a href="#" style="font-family: Nunito, sans-serif;">{{ Auth::user()->lastname }}</a>
                                             <span class="date" style="font-family: Nunito, sans-serif;">{{ date('jS M Y ', strtotime($news->created_at))}}</span></p>
                                             <img class="img-fluid" src="./images/{{ $news->img_path }}" style="background: url(&quot;https://cdn.bootstrapstudio.io/placeholders/1400x800.png&quot;);">
                                     </div>
@@ -89,93 +91,6 @@
         </div>
     </div>
 </div>
-
-<!--
-    <div>
-        <h1 class="" style="padding: 20px 0px;">Dashboard</h1>
-    </div>
-
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <strong>{{ $message }}</strong>
-        </div>
-    @elseif ($message = Session::get('error'))
-        <div class="alert alert-danger">
-            <strong>{{ $message }}</strong>
-        </div>
-    @endif
-
-    <div class="row">
-        <div class="col ">
-            <div class="card shadow">
-                <div class="card-header">
-                    <h3>News Feed</h3>
-                </div>
-
-                <div class="card-body p-5">
-                    @if (count($news_feed)>0)
-
-                    @foreach ($news_feed as $news)
-                        <div class="card shadow my-3">
-                            <div class="card-header">
-                                <h4 class="font-extrabold">
-                                    {{ $news->title }}
-                                </h4>
-
-                            </div>
-
-                            <div class="row card-body">
-                                <div class="col-4 text-center">
-                                    <img src="./images/{{ $news->img_path }}" alt="" width="200" >
-                                </div>
-
-                                <div class="m-auto sm:m-auto text-left col">
-                                    <div class="">
-                                        <p class="text fs-6  pb-9 ">
-                                            {{ Illuminate\Support\Str::limit($news->description, 100) }}
-                                        </p>
-                                    </div>
-
-
-                                    <p class="px-3 text fw-light ">
-                                        by <span class="fst-italic fw-bold ">{{ $news->user->name }}</span>
-                                        , Created on {{ date('jS M Y ', strtotime($news->created_at))}}
-                                    </p>
-
-
-
-                                    <a href="{{ route('news-readmore', $news->slug ) }}" class="shadow border border-secondary uppercase text-muted text-decoration-none float-end btn btn-light">
-                                        Read more..
-                                    </a>
-
-
-                                </div>
-                            </div>
-
-
-                        </div>
-                    @endforeach
-                    @else
-                    <h3>No News Available!</h3>
-                    @endif
-                </div>
-            </div>
-
-        </div>
-
-        <div class="col">
-            <div class="card shadow">
-                <div class="card-header">
-                    <h4>Calendar</h4>
-                </div>
-                <div class="card-body">
-
-                </div>
-            </div>
-        </div>
-    </div>
-
--->
 
 
 
