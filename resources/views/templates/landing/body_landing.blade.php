cfec96397f84467a4e376152165bff765371e0b7
mccescario <201811926@fit.edu.ph>
2022-03-31 20:54:20 +0800

    delete old buttons

--- resources/views/templates/landing/body_landing.blade.php
+++ resources/views/templates/landing/body_landing.blade.php
@@ -13,41 +13,6 @@
     </div>
 </header>
 
-<!--
- <header class="masthead" style="background-image: url('images/intro-bg.jpg')">
-            <div class="intro-body">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-8 mx-auto">
-                            <h1 class="brand-heading" style="color: var(--bs-white);">MATCHAPP</h1>
-                            <p class="intro-text" style="color: var(--bs-white);">A Sports and eSports Tournament Management App.</p>
-                            <a class="btn btn-link btn-circle" role="button" href="#MatchApp">
-                                <i class="fa fa-angle-double-down animated"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </header>
-        <section class="text-center content-section" id="MatchApp" style="background: #000000;">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-8 mx-auto">
-                        <a href="{{route('login')}}"><button class="button" type="button" ><span>SIGN IN</span></button></a>
-                        <button type="button" class="button" data-bs-toggle="modal" data-bs-target="#signin" data-hover="PROCEED">
-                            <span>SIGN IN</span>
-                        </button>
-
-                        <a href="{{route('register')}}"><button class="button" type="button" ><span>DON'T HAVE AN ACCOUNT?</span></button></a>
-                        <button type="button" class="button" data-bs-toggle="modal" data-bs-target="#register" data-hover="REGISTER">
-                            <span>DON'T HAVE AN ACCOUNT?</span>
-                        </button>
-
-                        <p style="margin: 40px;">&nbsp;MatchApp is a tournament management app specialized for sports and eSports available through web and mobile platform with key features such as tournament hosting, real-time management, livestreaming, and team matchmaking.</p>
-                    </div>
-                </div>
-            </div>
-        </section>-->
 
         <!--Login Modal-->
         <div class="modal fade" id="signin" tabindex="-1" aria-labelledby="signinLabel" aria-hidden="true">
