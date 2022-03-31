da2803c3f925964a34069b800f32c15e4e93434b
mccescario <201811926@fit.edu.ph>
2022-03-31 16:25:34 +0800

    calendar is deleted

--- resources/views/templates/host/dashboard.blade.php
+++ resources/views/templates/host/dashboard.blade.php
@@ -73,24 +73,11 @@
 
             </div>
         </div>
-        <div class="col-lg-5 col-xl-4">
-            <div class="card shadow mb-4">
-                <div class="card-header d-flex justify-content-between align-items-center">
-                    <h6 class="text-primary fw-bold m-0">Calendar</h6>
-                    <div class="dropdown no-arrow"><button class="btn btn-link btn-sm dropdown-toggle" aria-expanded="false" data-bs-toggle="dropdown" type="button"><i class="fas fa-ellipsis-v text-gray-400"></i></button>
-                        <div class="dropdown-menu shadow dropdown-menu-end animated--fade-in">
-                            <p class="text-center dropdown-header">dropdown header:</p><a class="dropdown-item" href="#">&nbsp;Action</a>
-                            <a class="dropdown-item" href="#">&nbsp;Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">&nbsp;Something else here</a>
-                        </div>
-                    </div>
-                </div>
-                    <!--Insert calendar-->
-            </div>
-        </div>
     </div>
 </div>
+
+
+
 
 
 
